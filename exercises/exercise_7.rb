a0e74780ce9491165445815144737347cbfdb875
gabmor38 <Gabriela.morenor@gmail.co>
2022-01-06 20:14:13 -0500

    completed verification for store and employee

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -22,3 +22,32 @@ puts "----------"
 # Ask the user for a store name (store it in a variable)
 # Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
 # Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validate :store_must_carry_apparel
+
+  def store_must_carry_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:store, "must carry at least one of the men's or women's apparel")
+    end
+  end
+end
+
+puts "Enter Store Name:"
+store_name = gets.chomp
+
+store = Store.create(name: store_name, annual_revenue: 1, mens_apparel: false, womens_apparel: false)
+
+puts store.errors.full_messages
+
